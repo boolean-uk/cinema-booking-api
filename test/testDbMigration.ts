1d1f7bb27d621377d57a5acf44fa95b936746323
Thomas Jensen <thomas.jensen_@outlook.com>
2024-08-15 14:52:23 +0100

    Fixed test:migration file

---
+++ test/testDbMigration.ts
@@ -0,0 +1,16 @@
+import { exec } from 'child_process'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+try {
+    // Connect to the user's test database instance
+    process.env['DATABASE_URL'] = process.env['TEST_DATABASE_URL']
+    console.log(`Connected to DB instance: ${process.env['DATABASE_URL']}`)
+
+    exec('npx prisma migrate reset --force', { encoding: 'utf-8' })
+    exec('npx prisma generate', { encoding: 'utf-8' })
+} catch (err) {
+    console.error(`Migration failed! \r\n ${err}`)
+    process.exit()
+}
