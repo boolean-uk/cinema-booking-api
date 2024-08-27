f743ef69f11da4d99271b125cc8df200ccdcbd8f
Atakan Temizkan <atikotemizkan@gmail.com>
2024-08-27 17:21:28 +0300

    added put request and get by id

--- src/routers/movies.js
+++ src/routers/movies.js
@@ -25,7 +25,31 @@ router.get("/", async (req, res) => {
   res.json({ movies: response });
 });
 
-router.get("/:id", async (req, res) => {});
+router.get("/:id", async (req, res) => {
+  try {
+    // TRY TO RUN THE ASYNC AWAIT FUNCTION
+    const [movie] = await prisma.movie.findMany({
+      // GET MOVIE WITH GIVEN ID
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+
+    // TRY TO GET THE SCREENINGS
+    const screenings = await prisma.screening.findMany({
+      // GET SCREENINGS FOR CURRENT MOVIE
+      where: { movieId: movie.id },
+    });
+
+    movie.screenings = screenings; // ADD SCREENINGS FIELD INTO THE CURRENT MOVIE OBJECT
+    // IF THERE ARE NO SCREENINGS IT WILL DISPLAY THE FOLLOWING MESSAGE
+
+    res.json({ movie });
+  } catch (error) {
+    // IF THERE ARE NO MOVIES THE AWAIT WILL THROW AN ERROR. THIS WILL CATCH IT
+    res.status(404).json({ error: "MOVIE NOT FOUND" });
+  }
+});
 
 // POST REQUESTS
 
@@ -46,6 +70,29 @@ router.post("/", async (req, res) => {
   movie.screenings = screening; // ADD SCREENINGS FIELD INTO THE CURRENT MOVIE OBJECT
 
   res.status(201).json({ movie: movie });
+});
+
+// PUT REQUESTS
+
+router.put("/:id", async (req, res) => {
+  try {
+    // TRY TO RUN THE ASYNC AWAIT FUNCTION
+    const movie = await prisma.movie.update({
+      // UPDATE MOVIE WITH GIVEN ID
+      where: {
+        id: Number(req.params.id),
+      },
+      data: {
+        title: req.body.title,
+        runtimeMins: req.body.runtimeMins,
+      },
+    });
+
+    res.status(201).json({ movie });
+  } catch (error) {
+    // IF THERE ARE NO MOVIES THE AWAIT WILL THROW AN ERROR. THIS WILL CATCH IT
+    res.status(404).json({ error: "MOVIE NOT FOUND" });
+  }
 });
 
 module.exports = router;
