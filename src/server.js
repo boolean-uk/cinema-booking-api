09f002cf85c9b1b71b18c6365200a3460a324ab7
Atakan Temizkan <atikotemizkan@gmail.com>
2024-08-27 19:36:48 +0300

    added customers section

--- src/server.js
+++ src/server.js
@@ -13,10 +13,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Tell express to use your routers here
-const customerRouter = require("./routers/customer");
+const customersRouter = require("./routers/customers");
 const moviesRouter = require("./routers/movies");
 const prisma = require("./utils/prisma");
-app.use("/customers", customerRouter);
+app.use("/customers", customersRouter);
 app.use("/movies", moviesRouter);
 
 module.exports = app;
